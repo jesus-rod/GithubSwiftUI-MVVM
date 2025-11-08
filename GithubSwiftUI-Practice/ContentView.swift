58f26f3b33bf9c3850adcc8e5fb4db048d893602
Jesús Rodríguez <rodriguezjesus609@gmail.com>
2025-11-08 04:13:54 +0100

    Initial Commit

---
+++ GithubSwiftUI-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubSwiftUI-Practice
+//
+//  Created by jesus on 08.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubSwiftUI-Practice/GithubSwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSwiftUI_PracticeApp.swift
+//  GithubSwiftUI-Practice
+//
+//  Created by jesus on 08.11.25.
+//
+
+import SwiftUI
+
+@main
+struct GithubSwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
